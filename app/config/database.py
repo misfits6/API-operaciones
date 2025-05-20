a0672ebf4fa27c41950ef2be2c2d023f1484337a
jhon <kbtotal90@gmail.com>
2025-05-20 11:30:44 -0500

    chore: remove sensitive database URI from configuration

--- app/config/database.py
+++ app/config/database.py
@@ -2,7 +2,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 
-uri = "mongodb+srv://jhonvargas:misfits6@pass-op-database.zmn5jo1.mongodb.net/?retryWrites=true&w=majority&appName=pass-op-database"
+uri = ""
 
 
 # Create a new client and connect to the server
